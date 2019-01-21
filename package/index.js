7b45868f7e85f06e788510d90300e2dc43c0177e
Zack Pace <zack@fbts.ca>
2019-01-21 16:29:48 -0600

    Update files.
    Co-Authored-By: Zack <Faztasio@users.noreply.github.com>

--- package/index.js
+++ package/index.js
@@ -1,4 +1,24 @@
 var glob = require("glob"),
-    fs = require("fs");
+    fs = require("fs"),
+    path = require("path"),
+    tynt = require("tynt");
 
-glob.glob('../bin/*', )
+glob.glob('../bin/*', {nonull: true}, function(er, files) {
+  if(er) {
+    throw er;
+  } else {
+    good = true;
+    for(i=0;i<files.length;i++) {
+      fs.copyFile(files[i], path.join(process.cwd(), files[i]), (err) => {
+        if (err) {
+          good = false;
+          throw err;
+        }
+        console.log(tynt.Green("File copied."));
+      });
+    }
+    if(good == true) {
+      console.log
+    }
+  }
+})
