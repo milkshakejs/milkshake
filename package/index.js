38840db40a5b768bc4f99c9241618d6d31a26ecf
Zack Pace <zack@fbts.ca>
2019-01-21 15:42:50 -0600

    Testing - Unstable

--- package/index.js
+++ package/index.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 
 gulp.task('moveBase',function(){
-    return gulp.src('../bin/*')
-        .pipe(gulp.dest('../../../'));
+  return gulp.src('../bin/*')
+         .pipe(gulp.dest('../../../'));
 });
 gulp.task('moveExtended',['moveBase'],function(){
-    return gulp.src('ExtendedFolder/*')
-        .pipe(gulp.dest('ResultFolder/'));
+  return gulp.src('../bin/*')
+         .pipe(gulp.dest('../../../'));
 });
 gulp.task('mergeFolders',['moveBase','moveExtended']);
