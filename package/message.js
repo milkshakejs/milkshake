805f5aa7066ec23bc7fbca68a596006bf50c7858
Zack Pace <zack@fbts.ca>
2019-01-21 16:45:05 -0600

    Application - Working

--- package/message.js
+++ package/message.js
@@ -1,14 +1,14 @@
 var M = {};
 
-M.Success = (message) {
+M.Success = function(message) {
   console.log("MILKSHAKE" + " " + tynt.Green("!SUCCESS") + " " + message + ".")
 }
 
-M.Error = (message) {
+M.Error = function(message) {
   console.log("MILKSHAKE" + " " + tynt.Red("!ERR") + " " + message + ".")
 }
 
-M.Warning = (message) {
+M.Warning = function(message) {
   console.log("MILKSHAKE" + " " + tynt.Yellow("!WARN") + " " + message + ".")
 }
 
