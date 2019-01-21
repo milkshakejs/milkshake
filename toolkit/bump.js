4eeb38d4c93695df96edd33ae2fd88e0491b1f4f
Zack Pace <zack@fbts.ca>
2019-01-21 14:42:28 -0600

    Update for real world usage.

--- toolkit/bump.js
+++ toolkit/bump.js
@@ -1,8 +1,8 @@
 var fs = require("fs");
 
-var package = JSON.parse(fs.readFileSync('package1.json'))
+var package = JSON.parse(fs.readFileSync('package.json'))
 var version = package.version.split(".")
 version[2] = (parseInt(version[2]) + 1).toString()
 package.version = version.join(".");
 
-fs.writeFileSync('package1.json', JSON.stringify(package))
+fs.writeFileSync('package.json', JSON.stringify(package))
