e873cfeecf2624b77679aff08184113a31157071
Zack Pace <zack@fbts.ca>
2019-01-21 14:47:26 -0600

    Update index.js

--- toolkit/index.js
+++ toolkit/index.js
@@ -1,17 +1,5 @@
 var toolkit = {};
 
-toolkit.Argument = function(arguments) {
-  var args;
-  if(!typeof arguments == "string") {
-    args = arguments.split(" ");
-  } else {
-    args = arguments;
-  }
-  for(i=0;i<args.length;i++) {
-    if(toolkit.Argument.prototype.Params[args[i]]) {
-      
-    }
-  }
-};
+toolkit.arguments
 
 module.exports = toolkit;
