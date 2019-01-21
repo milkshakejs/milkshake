7087732ca96d60d5060f38fd0453c1603fb7f954
Zack Pace <zack@fbts.ca>
2019-01-21 14:34:48 -0600

    Add flexibility

--- toolkit/index.js
+++ toolkit/index.js
@@ -3,7 +3,9 @@ var toolkit = {};
 toolkit.parse = function(arguments) {
   var args;
   if(!typeof arguments == "string") {
-    
+    args = arguments.split(" ");
+  } else {
+    args = arguments;
   }
 };
 
