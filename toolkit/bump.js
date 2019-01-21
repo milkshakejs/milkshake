fac8c86628c8c10a35c4a63e9a0215b591fa7458
Zack Pace <zack@fbts.ca>
2019-01-21 14:40:34 -0600

    Bump + update bump.js

--- toolkit/bump.js
+++ toolkit/bump.js
@@ -1,3 +1,7 @@
 var fs = require("fs");
 
 var package = JSON.parse(fs.readFileSync('package1.json'))
+var version = package.version.split(".")
+version[2] = (parseInt(version[2]) + 1).toString()
+
+fs.writeFileSync('package1.json', JSON.stringify(package))
