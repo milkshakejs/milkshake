7887f06014c612b67e6c72f38ae3895616d66073
Zack Pace <zack@fbts.ca>
2019-01-21 17:28:59 -0600

    Testing - Unstable

--- bin/publish-all.js
+++ bin/publish-all.js
@@ -1,3 +1,9 @@
 var cp = require("child_process");
 
-cp
+var fileName = "../publish-all";
+
+if(process.platform == "win32") {
+  cp.execFile(fileName + ".bat");
+} else {
+  cp.execFile(fileName + ".sh");
+}
