2c103f68c68998ed3a925e376aaaf4875627dfd9
Zack Pace <zack@fbts.ca>
2019-01-21 16:34:21 -0600

    Testing - Unstable

--- package/message.js
+++ package/message.js
@@ -1,1 +1,15 @@
 var M = {};
+
+M.Success = (message) {
+  console.log("MILKSHAKE" + " " + tynt.Green("!SUCCESS") + " " + message + ".")
+}
+
+M.Error = (message) {
+  console.log("MILKSHAKE" + " " + tynt.Red("!ERR") + " " + message + ".")
+}
+
+M.Warning = (message) {
+  console.log("MILKSHAKE" + " " + tynt.Yellow("!WARN") + " " + message + ".")
+}
+
+module.exports = M;
