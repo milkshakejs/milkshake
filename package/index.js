43c8f61b8e6c93b189c75b0821f5221c65f08092
Zack Pace <zack@fbts.ca>
2019-01-21 16:22:32 -0600

    Rework with glob
    Co-Authored-By: Zack <Faztasio@users.noreply.github.com>

--- package/index.js
+++ package/index.js
@@ -1,8 +1,4 @@
-var glob = require('glob'),
+var glob = require("glob"),
     fs = require("fs");
 
-gulp.task('web_dev', function () {
-    es.merge(gulp.src(path.resolve("../bin")),
-            gulp.src(process.cwd()))
-            .pipe(gulp.dest('out'));
-});
+glob.glob('../bin/*', )
