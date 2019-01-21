488483e424cfffa9eb9ee7603a5e60a0a86fc2e1
Zack Pace <zack@fbts.ca>
2019-01-21 17:03:20 -0600

    Testing - Working

--- package/index.js
+++ package/index.js
@@ -2,30 +2,8 @@ var glob = require("glob"),
     fs = require("fs"),
     path = require("path"),
     tynt = require("tynt"),
-    message = require("./message.js");
+    message = require("./message.js"),
+    merge = require("node-merge");
 
 message.tynt = tynt;
-
-glob.glob(path.resolve('../bin/*'), {nonull: true}, function(er, files) {
-  if(er) {
-    throw er;
-  } else {
-    good = true;
-    for(i=0;i<files.length;i++) {
-      fs.copyFile(files[i], path.join(process.cwd(), files[i]), (err) => {
-        if (err) {
-          good = false;
-          throw err;
-        }
-        message.Success("Installing " + path.basename(files[i]));
-      });
-    }
-    if(good == true) {
-      console.log();
-      message.Success("Installed successfully")
-    } else {
-      console.log();
-      message.Error("Install failed.  Some unneeded files may be in "  + process.cwd())
-    }
-  }
-})
+merge.mergeTo("../bin", process.cwd());
