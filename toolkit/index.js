7100532e8cedcd0d6b0f062eefe31df8e765c0e3
Zack Pace <zack@fbts.ca>
2019-01-21 14:56:27 -0600

    Update tools

--- toolkit/index.js
+++ toolkit/index.js
@@ -2,11 +2,14 @@ var toolkit = {};
 
 toolkits.ArgumentParser = {
   Parse: function(args) {
+    var result = {};
     if(typeof args == "string") {
       args = args.split(" ");
     }
     for(i=0;i<args.length;i++) {
-      
+      if(toolkits.ArgumentParser.Parameters[args[i]] == true) {
+        result[args[i].split("-").join("")] = args[i+1]
+      }
     }
   }
 }
---
+++ toolkit/test.js
@@ -0,0 +1,1 @@
+var toolkit = require("index.js")
