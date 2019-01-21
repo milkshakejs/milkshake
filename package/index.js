4a0fd040d12b2c4353a99bfd2309dce8191fdf56
Zack Pace <zack@fbts.ca>
2019-01-21 15:41:18 -0600

    Test - Working

--- package/index.js
+++ package/index.js
@@ -1,2 +1,11 @@
-var mergedirs = require("merge-dirs")
-mergedirs("../bin", "../../..", "skip")
+var gulp = require('gulp');
+
+gulp.task('moveBase',function(){
+    return gulp.src('../bin/*')
+        .pipe(gulp.dest('../../../'));
+});
+gulp.task('moveExtended',['moveBase'],function(){
+    return gulp.src('ExtendedFolder/*')
+        .pipe(gulp.dest('ResultFolder/'));
+});
+gulp.task('mergeFolders',['moveBase','moveExtended']);
