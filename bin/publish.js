a824332332939816d12045262b61110dc0b875fd
Zack Pace <zack@fbts.ca>
2019-01-22 09:43:52 -0600

    Testing - Unstable

--- bin/publish.js
+++ bin/publish.js
@@ -43,7 +43,7 @@ if(safe) {
     cp.exec("git add . && git commit -m \"Debug - Unstable\" && git push origin master && npm publish --access public");
   }
   bump();
-} else if(bug-fix) {
+} else if(bug_fix) {
   if(release) {
     cp.exec("git add . && git commit -m \"Release - Bug Fix\" && git push origin master && npm publish --access public");
   } else if(debug) {
