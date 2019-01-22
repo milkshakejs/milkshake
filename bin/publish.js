f05d6d6935cdc13f2dcac1a5f8788debec1d770d
Zack Pace <zack@fbts.ca>
2019-01-22 09:36:48 -0600

    Testing - Unstable

--- bin/publish.js
+++ bin/publish.js
@@ -22,28 +22,34 @@ var testing = process.argv[2] == "testing";
 var unstable = process.argv[2] == "unstable";
 var bug-fix = process.argv[2] == "bug-fix";
 
+var no-bump
+
 if(safe) {
   if(release) {
     cp.exec("git add . && git commit -m \"Release - Safe\" && git push origin master && npm publish --access public");
   } else if(debug) {
     cp.exec("git add . && git commit -m \"Debug - Safe\" && git push origin master && npm publish --access public");
   }
+  bump();
 } else if(testing) {
   if(release) {
     cp.exec("git add . && git commit -m \"Release - Testing\" && git push origin master && npm publish --access public");
   } else if(debug) {
     cp.exec("git add . && git commit -m \"Debug - Testing\" && git push origin master && npm publish --access public");
   }
+  bump();
 } else if(unstable) {
   if(release) {
     cp.exec("git add . && git commit -m \"Release - Unstable\" && git push origin master && npm publish --access public");
   } else if(debug) {
     cp.exec("git add . && git commit -m \"Debug - Unstable\" && git push origin master && npm publish --access public");
   }
+  bump();
 } else if(bug-fix) {
   if(release) {
     cp.exec("git add . && git commit -m \"Release - Bug Fix\" && git push origin master && npm publish --access public");
   } else if(debug) {
     cp.exec("git add . && git commit -m \"Debug - Bug Fix\" && git push origin master && npm publish --access public");
   }
+  bump();
 }
