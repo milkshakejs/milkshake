f3e2561a8cbad10d6900d2245fbbe4285df5c12b
Zack Pace <zack@fbts.ca>
2019-01-21 20:54:07 -0600

    Testing - Unstable

--- bin/publish.js
+++ bin/publish.js
@@ -1,7 +1,23 @@
-var cp = require("child_process");
+var cp = require("child_process"),
+    path = require("path"),
+    fs = require("fs");
+
+var bump = function() {
+  var fs = require("fs");
+
+  var package = JSON.parse(fs.readFileSync(path.join('package.json')))
+  var version = package.version.split(".")
+  version[2] = (parseInt(version[2]) + 1).toString()
+  package.version = version.join(".");
+
+  fs.writeFileSync('package.json', JSON.stringify(package))
+  // For MSJS
+}
 
 var fileName = "../publish-all";
 
 var safe = process.argv[1] == "safe"; if(safe) fileName = "./binsh/publish-safe";
 var testing = process.argv[1] == "testing"; if(safe) fileName = "./binsh/publish-testing";
 var safe = process.argv[1] == "unstable"; if(safe) fileName = "./binsh/publish-unstable";
+
+cp.exec("")
