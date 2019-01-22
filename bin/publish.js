7077146b1a7ca45e5f73a01f33335c06be09cb89
Zack Pace <zack@fbts.ca>
2019-01-21 20:51:20 -0600

    Testing - Unstable

--- bin/publish.js
+++ bin/publish.js
@@ -5,9 +5,3 @@ var fileName = "../publish-all";
 var safe = process.argv[1] == "safe"; if(safe) fileName = "./binsh/publish-safe";
 var testing = process.argv[1] == "testing"; if(safe) fileName = "./binsh/publish-testing";
 var safe = process.argv[1] == "unstable"; if(safe) fileName = "./binsh/publish-unstable";
-
-if(process.platform == "win32") {
-  cp.execFile(fileName + ".bat", [process.cwd()]);
-} else {
-  cp.execFile(fileName + ".sh", [process.cwd()]);
-}
