3c5015a49b4c944f15ec48f61087848af302673a
Zack Pace <zack@fbts.ca>
2019-01-22 10:23:32 -0600

    Testing - Unstable

--- bin/unpack.js
+++ bin/unpack.js
@@ -1,2 +1,4 @@
 var fs = require("fs"),
     unzip = require("unzip");
+    version = fs.readFileSync("version.txt");
+fs.createReadStream('binary-' + version.split("\n")[0] + ".zip").pipe(unzip.Extract({ path: process.cwd() }));
