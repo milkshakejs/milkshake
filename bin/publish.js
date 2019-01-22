024fc22c028f20f713e5c802fc1beb6d6a07297a
Zack Pace <zack@fbts.ca>
2019-01-21 21:03:37 -0600

    Testing - Working

--- bin/publish.js
+++ bin/publish.js
@@ -14,9 +14,9 @@ var bump = function() {
 
 var fileName = "../publish-all";
 
-var safe = process.argv[1] == "safe"; if(safe) fileName = "./binsh/publish-safe";
-var testing = process.argv[1] == "testing"; if(safe) fileName = "./binsh/publish-testing";
-var safe = process.argv[1] == "unstable"; if(safe) fileName = "./binsh/publish-unstable";
+var safe = process.argv[1] == "safe"; if(safe) {fileName = "./binsh/publish-safe"};
+var testing = process.argv[1] == "testing"; if(safe) {fileName = "./binsh/publish-testing"};
+var safe = process.argv[1] == "unstable"; if(safe) {fileName = "./binsh/publish-unstable"};
 
 if(process.platform == "win32") {
   cp.exec(fs.readFileSync(path.join("./binsh", fileName + ".bat")).split("\n").join(" && "));
